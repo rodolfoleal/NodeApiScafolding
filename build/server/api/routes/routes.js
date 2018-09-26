c4036ba306dcfd814166d08eae8a11de6b189f39
Rodolfo Leal <rodolfo.pk@gmail.com>
2018-09-25 22:47:46 -0300

    adding user routes

--- build/server/api/routes/routes.js
+++ build/server/api/routes/routes.js
@@ -1,12 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var routes_1 = require("../../modules/user/routes");
 var Routes = /** @class */ (function () {
     function Routes(app) {
+        this.router = new routes_1.default;
         this.getRoutes(app);
     }
     Routes.prototype.getRoutes = function (app) {
-        app.route('/').get(function (req, res) { return res.send('Hello World'); });
-        app.route('/ola/:nome').get(function (req, res) { return res.send("Hello " + req.params.nome); });
+        app.route('/api/users').get(this.router.index);
+        app.route('/api/users/create').post(this.router.create);
+        app.route('/api/users/:id').get(this.router.findOne);
+        app.route('/api/users/:id').put(this.router.update);
+        app.route('/api/users/:id').delete(this.router.delete);
     };
     return Routes;
 }());
--- server/api/routes/routes.ts
+++ server/api/routes/routes.ts
@@ -1,14 +1,21 @@
 import { Application, Request, Response } from 'express';
+import UserRoutes from '../../modules/user/routes';
 
 class Routes {
 
+    private router: UserRoutes;
+    
     constructor(app: Application) {
+        this.router = new UserRoutes;
         this.getRoutes(app);
     }
 
     getRoutes(app: Application): void {
-        app.route('/').get((req: Request, res: Response) => res.send('Hello World'));
-        app.route('/ola/:nome').get((req: Request, res: Response) => res.send(`Hello ${req.params.nome}`))
+        app.route('/api/users').get(this.router.index);
+        app.route('/api/users/create').post(this.router.create);
+        app.route('/api/users/:id').get(this.router.findOne);
+        app.route('/api/users/:id').put(this.router.update);
+        app.route('/api/users/:id').delete(this.router.delete);
     }
 }
 
